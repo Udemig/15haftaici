918a21035875ca3efc1bd28981d9141f7260fbe6
isveckrali <mehmet.can.seyhan1@gmail.com>
2026-02-04 21:59:48 +0100

    js init

---
+++ 13-JSDers01/main.js
@@ -0,0 +1,51 @@
+document.writeln("Merhaba Dunya" + "</br>")
+
+var ad = "memo"
+var ad = "memo1"
+ad = "memo2"
+
+document.writeln("adii: " + ad + "</br>")
+
+let soyad = "can"
+soyad = "refia"
+
+// let soyad = "refia"
+document.writeln("soyad: " + soyad + "</br>")
+
+const TCNo = 30285392523
+document.writeln("TCNo: " + TCNo + "</br>")
+
+
+let adSoyadVeYas = "mehmet can seyhan 30"
+
+let sayi1 = 2
+let sayi2 = 4 
+
+document.writeln("- - - -- - -- - - - -- - - - -- - - -: "  + "</br>") 
+
+document.writeln("sayi1: " + sayi1 + "</br>")
+
+document.writeln("sayi2: " + sayi2 + "</br>")
+
+document.writeln("- - - -- - -- - - - -- - - - -- - - -: "  + "</br>") 
+
+let toplam = sayi1 + sayi2
+
+document.writeln("toplam: " + toplam + "</br>")
+
+let cikar = sayi2 - sayi1
+document.writeln("cikar: " + cikar + "</br>")
+
+let carp = sayi1 * sayi2
+document.writeln("carp: " + carp + "</br>")
+
+let bolme = sayi2 / sayi2
+document.writeln("bolme: " + bolme + "</br>")
+
+let modAlma = sayi2 % sayi1
+document.writeln("modAlma: " + modAlma + "</br>")
+
+
+
+
+
