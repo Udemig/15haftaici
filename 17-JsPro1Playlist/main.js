f794706b3f1e126cb2168d8ee37e5484fe1df42d
isveckrali <mehmet.can.seyhan1@gmail.com>
2026-02-26 19:24:11 +0100

    playlist initialized

--- 17-JsPro1Playlist/main.js
+++ 17-JsPro1Playlist/main.js
@@ -21,3 +21,239 @@ const closeButton = document.getElementById('close-button')
 const playListSongs = document.getElementById('playlist-songs')
 
 const currentProgress = document.getElementById('current-progress')
+
+
+//sira
+let index
+
+//dongu
+let loop = true
+
+//sarki listesi
+const songsList = [
+    {
+        name: "Gelo Ew Ki Bu",
+        link: "assets/gelo-ew-ki-bu.mp3",
+        artist: "Aram Tigran",
+        image: "assets/aram-tigran.jpeg"
+    },
+    {
+        name: "Gitme Kal",
+        link: "assets/yara-bere-icindeyim.mp3",
+        artist: "Hira-i Zerdust",
+        image: "assets/hirai.jpeg"
+    },
+    {
+        name: "Aramam",
+        link: "assets/aramam.mp3",
+        artist: "Ibrahim Tatlises",
+        image: "assets/ibrahim-tatlises.jpeg"
+    },
+    {
+        name: "Ax Eman",
+        link: "assets/ax-eman.mp3",
+        artist: "Rewsan Celiker",
+        image: "assets/rewsan-celiker.jpeg"
+    },
+    {
+        name: "Dinle",
+        link: "assets/dinle.mp3",
+        artist: "Mahsun Kirmizigul",
+        image: "assets/mahsun.jpeg"
+    }
+]
+
+//zaman formatla
+const timeFormatter = (timeInput) => {
+    let minute = Math.floor(timeInput / 60)
+    minute = minute < 10 ? "0"+minute : minute
+    let second = Math.floor(timeInput % 60)
+    second = second < 10 ? "0"+second : second
+    return `${minute}:${second}`
+}
+
+
+//sarkiyi ata
+const setSong = (arrayIndex) => {
+    console.log(arrayIndex)
+
+    let {name, link, artist, image} = songsList[arrayIndex]
+
+    audio.src = link
+    songName.innerHTML = name
+    songArtist.innerHTML = artist
+    songImage.src = image
+
+    audio.onloadeddata = () =>{
+        maxDuration.innerText = timeFormatter(audio.duration)
+    }
+
+    playAudio()
+    
+    playListContainer.classList.add("hide")
+
+}
+
+//sure gectikce
+audio.addEventListener("timeupdate",()=>{
+    currentTimeRef.innerText = timeFormatter(audio.currentTime)
+
+    let value = (audio.currentTime / audio.duration.toFixed(3)) * 100 + "%"
+    currentProgress.style.width = value
+})
+
+
+progressBar.addEventListener("click",(event) => {
+    //balangic
+    let coordStart = progressBar.getBoundingClientRect().left
+
+    console.log(coordStart)
+
+    let coordEnd = event.clientX
+
+    console.log(coordEnd)
+
+    console.log(progressBar.offsetWidth)
+
+    let progress = (coordEnd - coordStart) / progressBar.offsetWidth
+
+    console.log(progress)
+
+    currentProgress.style.width = progress * 100 + "%"
+    audio.currentTime = progress * audio.duration
+
+    playAudio()
+
+})
+
+//sarki listesi ac
+playListButton.addEventListener("click", ()=>{
+    playListContainer.classList.remove("hide")
+})
+
+//sarki listesini kapat
+closeButton.addEventListener("click",()=>{
+    playListContainer.classList.add("hide")
+})
+
+
+//sarki bittiginde tetiklenir
+audio.onended = () => {
+    nextSong()
+}
+
+
+
+//sarkiyi cal
+const playAudio = () =>{
+    audio.play()
+
+    playButton.classList.add("hide")
+    pauseButton.classList.remove("hide")
+}
+
+
+//sarkiyi durdur
+const pauseAudio = () =>{
+    audio.pause()
+
+    playButton.classList.remove("hide")
+    pauseButton.classList.add("hide")
+
+}
+
+//siradaki sarki
+const nextSong = () =>{
+
+    if (loop) {
+        if (index == (songsList.length - 1)) {
+            index = 0
+        } else {
+            index = index + 1
+        }
+    } else {
+        let randIndex = Math.floor(Math.random() * songsList.length)
+        index = randIndex
+    }
+
+    setSong(index)
+}
+
+
+//onceki sarkiya gec
+const previousSong = () => {
+    if (index > 0) {
+        index = index - 1
+    } else {
+        index = songsList.length - 1
+    }
+
+    setSong(index)
+}
+
+//ekran tuklendiginde
+window.onload = () => {
+    index = 0
+    setSong(index)
+    pauseAudio()
+    initializePlaylist()
+}
+
+//sarki listesi olustur
+const initializePlaylist = () => {
+    for (const i in songsList){
+
+        playListSongs.innerHTML += 
+        `<li class="playlistSong"
+            onclick="setSong(${i})">
+                <div class="playlist-image-container">
+                    <img src="${songsList[i].image}" />
+                </div>
+                <div class="playlist-song-details">
+                    <span id="playlist-song-name">
+                        ${songsList[i].name}
+                    </span>
+                    <span id="playlist-song-artist-album">
+                        ${songsList[i].artist}
+                    </span>
+                </div>
+            </li> `
+    }
+}
+
+//dongu butonuna tiklanilirsa
+repeatButton.addEventListener("click",()=>{
+    if (repeatButton.classList.contains("active")) {
+        repeatButton.classList.remove("active")
+        loop = false
+    } else {
+        repeatButton.classList.add("active")
+        loop = true
+    }
+})
+
+//karistirici tiklanirsa
+shuffleButton.addEventListener("click",()=>{
+     if (shuffleButton.classList.contains("active")) {
+        shuffleButton.classList.remove("active")
+        loop = true
+    } else {
+        shuffleButton.classList.add("active")
+        loop = false
+    }
+})
+
+
+//tiklanama dinleyecileri
+
+//durdur
+pauseButton.addEventListener("click",pauseAudio)
+
+//oynat
+playButton.addEventListener("click",playAudio)
+
+//siradaki tiklanirsa
+nextButton.addEventListener("click",nextSong)
+
+//onceki tiklanirsa
+prevButton.addEventListener("click",previousSong)
